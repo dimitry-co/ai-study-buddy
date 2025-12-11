73ff9603825c018bd86bd2650d74a968602f87a0
dimitry-co <dimitrygallego@gmail.com>
2025-12-11 03:38:42 -0500

    change constant MIN_QUESTIONS to 0. temporary fix because when min. was 5, user can't delete 5 and get stuck on 5.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,7 +9,7 @@ const FREE_GENERATION_LIMIT = 4;
 
 // Question generation limits
 const MAX_QUESTIONS = 60;  // Keeps API response time reasonable (~30 seconds)
-const MIN_QUESTIONS = 5;
+const MIN_QUESTIONS = 0;
 
 // File size limits (in bytes)
 const MAX_FILE_SIZE_MB = 20;
